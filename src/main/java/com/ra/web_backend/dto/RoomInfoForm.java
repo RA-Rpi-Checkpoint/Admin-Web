14640b9021ea667c7caaf0027b740814d6e15650
Seongwon97 <57744251+Seongwon97@users.noreply.github.com>
2021-09-08 15:50:24 +0900

    초기 DB설정 추가
    초기값은 RoomNumber와 HumanCount가 0으로 초기화

--- src/main/java/com/ra/web_backend/dto/RoomInfoForm.java
+++ src/main/java/com/ra/web_backend/dto/RoomInfoForm.java
@@ -1,11 +1,14 @@
 package com.ra.web_backend.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.ra.web_backend.entity.RoomInfo;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
 @ToString
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class RoomInfoForm {
     private int robotNum;
     private int roomNum;
---
+++ src/main/resources/h2/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS room_info (
+    robot_num INT NOT NULL,
+    room_num INT,
+    human_count INT,
+    PRIMARY KEY(robot_num)
+);

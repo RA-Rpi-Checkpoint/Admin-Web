0863e21e9257371e98fab6e2401bac347cae98be
Seongwon97 <osw970325@naver.com>
2021-09-22 20:11:37 +0900

    Add Test Case
    local에서 Test결과 잘 돌아감. 하지만 초기 DB값을 넣는데 오류가 여전히 발생한다.

--- src/test/java/com/ra/web_backend/repository/RobotInfoRepositoryTest.java
+++ src/test/java/com/ra/web_backend/repository/RobotInfoRepositoryTest.java
@@ -4,6 +4,7 @@ import com.ra.web_backend.entity.RobotInfo;
 import com.ra.web_backend.entity.RoomInfo;
 import com.ra.web_backend.entity.status.RobotStatus;
 import com.ra.web_backend.entity.status.RoomStatus;
+import com.ra.web_backend.service.RoomRobotService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,34 +18,69 @@ class RobotInfoRepositoryTest {
     @Autowired
     private RoomInfoRepository roomInfoRepository;
 
+    @Autowired
+    private RoomRobotService roomRobotService;
+
     @Test
-    void robotFindTest() {
-        System.out.println("Test");
+    void test1() {
         RobotInfo robotInfo = new RobotInfo();
         robotInfo.setRobotId(1);
-        robotInfo.setRecentRoom(2);
-        robotInfo.setStatus(RobotStatus.COMPLETE);
-
+        robotInfo.setRecentRoom(0);
+        robotInfo.setStatus(RobotStatus.RUNNING);
         robotInfoRepository.save(robotInfo);
 
         robotInfo.setRobotId(2);
-        robotInfo.setRecentRoom(2);
-        robotInfo.setStatus(RobotStatus.COMPLETE);
-
+        robotInfo.setRecentRoom(0);
+        robotInfo.setStatus(RobotStatus.WAITING);
         robotInfoRepository.save(robotInfo);
-        robotInfoRepository.findAll().forEach(System.out::println);
+
+        RoomInfo roomInfo = new RoomInfo();
+        roomInfo.setRoomId(1);
+        roomInfo.setHumanCount(0);
+        roomInfo.setStatus(RoomStatus.PENDING);
+        roomInfoRepository.save(roomInfo);
+
+        roomInfo.setRoomId(2);
+        roomInfo.setHumanCount(0);
+        roomInfo.setStatus(RoomStatus.PENDING);
+        roomInfoRepository.save(roomInfo);
+
+        roomInfo.setRoomId(3);
+        roomInfo.setHumanCount(0);
+        roomInfo.setStatus(RoomStatus.PENDING);
+        roomInfoRepository.save(roomInfo);
+
+        roomInfo.setRoomId(4);
+        roomInfo.setHumanCount(0);
+        roomInfo.setStatus(RoomStatus.PENDING);
+        roomInfoRepository.save(roomInfo);
+
+        roomInfo.setRoomId(5);
+        roomInfo.setHumanCount(0);
+        roomInfo.setStatus(RoomStatus.PENDING);
+        roomInfoRepository.save(roomInfo);
+
+        roomInfo.setRoomId(6);
+        roomInfo.setHumanCount(0);
+        roomInfo.setStatus(RoomStatus.PENDING);
+        roomInfoRepository.save(roomInfo);
+
+    }
+
+    @Test
+    void robotTest() throws Exception {
+        int robotNum = 1;
+        int roomNum =2;
+        RobotStatus status = RobotStatus.RUNNING;
+        int humanCount =2;
+        roomRobotService.editRobotInfo(robotNum, roomNum, status);
+        roomRobotService.editRoomInfo(roomNum, humanCount);
 
     }
 
     @Test
     void roomTest() {
-        RoomInfo roomInfo = new RoomInfo();
-        roomInfo.setRoomId(2);
-        roomInfo.setHumanCount(2);
-        roomInfo.setStatus(RoomStatus.PENDING);
-        roomInfoRepository.save(roomInfo);
 
-        roomInfoRepository.findAll().forEach(System.out::println);
 
     }
 }
--- src/test/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into roominfo (room_id, human_count, status, created_at, updated_at) values (1, 0, 'PENDING', now(), now());
-insert into roominfo (room_id, human_count, status, created_at, updated_at) values (2, 0, 'PENDING', now(), now());
-insert into roominfo (room_id, human_count, status, created_at, updated_at) values (3, 0, 'PENDING', now(), now());
-insert into roominfo (room_id, human_count, status, created_at, updated_at) values (4, 0, 'PENDING', now(), now());
-insert into roominfo (room_id, human_count, status, created_at, updated_at) values (5, 0, 'PENDING', now(), now());
-insert into roominfo (room_id, human_count, status, created_at, updated_at) values (6, 0, 'PENDING', now(), now());
